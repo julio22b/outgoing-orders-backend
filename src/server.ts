3eec5c7b20b6628f6edadb97cffa245524c165f0
julio <jbermudezfc90@gmail.com>
2026-06-08 22:45:47 -0400

    add patch to methods

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const allowedOrigins = process.env.FRONTEND_URL
     ? [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://127.0.0.1:5173']
     : ['http://localhost:5173', 'http://127.0.0.1:5173'];
 
-const methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'];
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'];
 
 const io = new Server(httpServer, {
     cors: {
